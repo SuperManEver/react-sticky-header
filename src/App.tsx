509854cbb949b2fb9659b647644946a7421da979
Nik Luparev <nikita.luparev@gmail.com>
2024-03-13 10:36:34 +0500

    some progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import cn from 'clsx';
 
 import useScrollDirection from './utils/use-scroll-direction';
 
+import './global.css';
+
 import css from './styles.module.scss';
 
 /**
@@ -17,17 +19,37 @@ import css from './styles.module.scss';
  *
  */
 
-function useStickyHeader() {
-  const [headerStickyVisible, setHeaderVisibility] = useState(false);
+const HEADER_HEIGHT = 80;
+
+function getDistanceToTop() {
+  return (
+    window.scrollY ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0
+  );
+}
+
+function useStickyHeader(scrollDirection: 'up' | 'down' | null) {
+  const [isSticky, setSticky] = useState(false);
   const headerRef = useRef<HTMLElement>(null);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const toggleSticky = () => {
-    if (window.scrollY > 80) {
-      setHeaderVisibility(true);
+    if (headerRef && headerRef.current) {
+      const rect = headerRef.current.getBoundingClientRect();
 
-      if (headerRef && headerRef.current) {
-        headerRef.current.style.display = 'none';
+      if (scrollDirection === 'down') {
+        if (getDistanceToTop() > HEADER_HEIGHT) {
+          setSticky(true);
+        }
+      }
+
+      // it means we reached TOP but scrolling up
+      if (scrollDirection === 'up') {
+        if (rect.top === 0 && getDistanceToTop() < 10) {
+          setSticky(false);
+        }
       }
     }
   };
@@ -40,48 +62,44 @@ function useStickyHeader() {
     };
   }, [toggleSticky]);
 
-  return { headerRef, headerStickyVisible };
+  return { headerRef, isSticky };
 }
 
 function App() {
   const scrollDirection = useScrollDirection();
-  const { headerRef, headerStickyVisible } = useStickyHeader();
+  const { headerRef, isSticky } = useStickyHeader(scrollDirection);
 
   const belowThreshold = window.scrollY > 82;
 
-  const headerCn = cn(css.header, {
-    [css.sticky]: belowThreshold,
-    [css.visible]: scrollDirection === 'up' && belowThreshold,
-    [css.hidden]: scrollDirection === 'down' && belowThreshold,
-  });
   const rootCn = cn(css.root);
-
-  // console.log('isSticky: ', isSticky);
+  const headerCn = cn(css.header, {
+    [css.sticky]: isSticky,
+    [css.visible]: isSticky && scrollDirection === 'up',
+    [css.hidden]: isSticky && scrollDirection === 'down',
+  });
 
   const valuesCn = cn(css.values, { [css.sticky]: belowThreshold });
-
-  const stickyHeader = cn(css.stickyHeader);
 
   return (
     <div className={rootCn}>
       <div className={valuesCn}>
-        <p>{window.scrollY}</p>
+        <p>window.scrollY: {window.scrollY}</p>
+        <p>
+          document.documentElement.scrollTop:{' '}
+          {document.documentElement.scrollTop}
+        </p>
         <p>{scrollDirection}</p>
-        <p>{headerStickyVisible ? 'visible' : 'hidden'}</p>
+        <p>{isSticky ? 'STICKY' : 'NOT STICKY'}</p>
+
+        {headerRef && headerRef.current && <p>{headerRef.current.style}</p>}
       </div>
 
       <header ref={headerRef} className={headerCn}>
         HEADER
       </header>
 
-      {headerStickyVisible && (
-        <header className={stickyHeader}>STICKY HEADER</header>
-      )}
-
       {/* {isSticky && <header className={css.stickyHeader}>HEADER</header>} */}
       <section className={css.section}>SECTION </section>
-      <aside className={css.aside}>ASIDE</aside>
-      <footer className={css.footer}>FOOTER</footer>
     </div>
   );
 }
